f2b552153d348b1ae4fa43198de5d8a6d558da7c
Valentin NOEL <valentin.noel@media-io.com>
2022-04-12 16:27:19 +0200

    libkdudec: fixes the output picture data

--- libavcodec/libkdudec.c
+++ libavcodec/libkdudec.c
@@ -18,6 +18,48 @@ typedef struct LibKduContext {
     AVClass *class;
 } LibKduContext;
 
+static inline int libkdu_are_pixel_components_packed(enum AVPixelFormat pix_fmt)
+{
+    const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(pix_fmt);
+    int i, component_plane;
+
+    if (pix_fmt == AV_PIX_FMT_GRAY16) {
+        printf("Pixel GRAY16!\n");
+        return 0;
+    }
+
+    component_plane = desc->comp[0].plane;
+    printf("Component 0: component_plane=%d\n", component_plane);
+    for (i = 1; i < desc->nb_components; i++) {
+        printf("Component %d: component_plane=%d\n", i, desc->comp[i].plane);
+        if (component_plane != desc->comp[i].plane)
+            return 0;
+    }
+    return 1;
+}
+
+static inline int libkdu_get_bytes_per_pixel(enum AVPixelFormat pix_fmt)
+{
+    const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(pix_fmt);
+    return desc->comp[0].step;
+}
+
+static inline void libkdu_copy_to_packed_8(AVFrame *picture, const uint8_t *data, int nb_components)
+{
+    uint8_t *img_ptr;
+    int x, y, c;
+    int index = 0;
+
+    for (y = 0; y < picture->height; y++) {
+        img_ptr = picture->data[0] + y * picture->linesize[0];
+        for (x = 0; x < picture->width; x++) {
+            for (c = 0; c < nb_components; c++) {
+                *img_ptr++ = data[index++];
+            }
+        }
+    }
+}
+
 static av_cold int libkdu_decode_init(AVCodecContext *avctx)
 {
     // LibKduContext *ctx = avctx->priv_data;
@@ -33,10 +75,11 @@ static int libkdu_decode_frame(AVCodecContext *avctx, void *data, int *got_frame
     int buf_size = avpkt->size;
     // LibKduContext *ctx = avctx->priv_data;
     AVFrame *picture = data;
+    uint8_t* buffer;
 
-    int width, height, num_comps, ret;
+    int width, height, num_comps, nb_pixels, bytes_per_pixel, ret;
     int *stripe_heights;
-    int pull_strip_should_stop = 0;
+    int pull_strip_should_stop, are_components_packed = 0;
 
     kdu_compressed_source *source;
     kdu_codestream *code_stream;
@@ -81,6 +124,10 @@ static int libkdu_decode_frame(AVCodecContext *avctx, void *data, int *got_frame
         goto done;
     }
 
+    // Initialize output picture buffer
+    nb_pixels = width * height * num_comps;
+    buffer = av_malloc(nb_pixels);
+
     // Initialize the output picture component stripes
     stripe_heights = av_malloc(num_comps * sizeof(int));
     for (int i = 0; i < num_comps; ++i) {
@@ -90,7 +137,52 @@ static int libkdu_decode_frame(AVCodecContext *avctx, void *data, int *got_frame
     // Start decoding the stripes
     kdu_stripe_decompressor_start(decompressor, code_stream);
     while (!pull_strip_should_stop) {
-        pull_strip_should_stop = kdu_stripe_decompressor_pull_stripe(decompressor, picture->data[0], stripe_heights);
+        pull_strip_should_stop = kdu_stripe_decompressor_pull_stripe(decompressor, buffer, stripe_heights);
+    }
+
+    // Get number of bytes per pixel
+    bytes_per_pixel = libkdu_get_bytes_per_pixel(avctx->pix_fmt);
+
+    // Are components interlaced (packed) or planar?
+    are_components_packed   = libkdu_are_pixel_components_packed(avctx->pix_fmt);
+
+    switch (bytes_per_pixel) {
+        case 1:
+            if (are_components_packed) {
+                libkdu_copy_to_packed_8(picture, buffer, num_comps);
+            } else {
+                av_log(avctx, AV_LOG_ERROR, "Copy to 8 unimplemented!");
+                ret = 0;
+                goto done;
+            }
+            break;
+        case 2:
+            if (are_components_packed) {
+                libkdu_copy_to_packed_8(picture, buffer, num_comps);
+            } else {
+                av_log(avctx, AV_LOG_ERROR, "Copy to 16 unimplemented!");
+                ret = 0;
+                goto done;
+            }
+            break;
+        case 3:
+        case 4:
+            if (are_components_packed) {
+                libkdu_copy_to_packed_8(picture, buffer, num_comps);
+            }
+            break;
+        case 6:
+        case 8:
+            if (are_components_packed) {
+                av_log(avctx, AV_LOG_ERROR, "Copy to packed 16 unimplemented!");
+                ret = 0;
+                goto done;
+            }
+            break;
+        default:
+            avpriv_report_missing_feature(avctx, "Pixel size %d", bytes_per_pixel);
+            ret = AVERROR_PATCHWELCOME;
+            goto done;
     }
 
     // End decoding the stripes
@@ -107,6 +199,7 @@ static int libkdu_decode_frame(AVCodecContext *avctx, void *data, int *got_frame
 
 done:
     // Clean and return
+    av_free(buffer);
     av_free(stripe_heights);
     kdu_stripe_decompressor_delete(decompressor);
     kdu_codestream_delete(code_stream);
