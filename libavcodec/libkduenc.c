943a8ecd4a435b5129863bed5002dd48840c5515
Valentin NOEL <valentin.noel@media-io.com>
2022-04-15 10:22:11 +0200

    First version of libkduenc

--- libavcodec/libkduenc.c
+++ libavcodec/libkduenc.c
@@ -4,14 +4,33 @@
  */
 
 #include "libavutil/common.h"
+#include "libavutil/imgutils.h"
 #include "libavutil/opt.h"
+
 #include "avcodec.h"
 #include "codec_internal.h"
+
 #include <kduc.h>
 
 typedef struct LibKduContext {
     AVClass *avclass;
 } LibKduContext;
+
+static inline void libkdu_copy_from_packed_8(uint8_t *data, const AVFrame *frame, int nb_components)
+{
+    uint8_t *img_ptr;
+    int x, y, c;
+    int index = 0;
+
+    for (y = 0; y < frame->height; y++) {
+        img_ptr = frame->data[0] + y * frame->linesize[0];
+        for (x = 0; x < frame->width; x++) {
+            for (c = 0; c < nb_components; c++) {
+                data[index++] = *img_ptr++;
+            }
+        }
+    }
+}
 
 static av_cold int libkdu_encode_init(AVCodecContext *avctx)
 {
@@ -22,16 +41,85 @@ static av_cold int libkdu_encode_init(AVCodecContext *avctx)
     return 0;
 }
 
-static int libkdu_encode_frame(AVCodecContext *avctx, AVPacket *pkt,
-                                    const AVFrame *frame, int *got_packet)
+static int libkdu_encode_frame(AVCodecContext *avctx, AVPacket *pkt, const AVFrame *frame, int *got_packet)
 {
-    LibKduContext *ctx = avctx->priv_data;
+    LibKduContext* ctx = avctx->priv_data;
+    const AVPixFmtDescriptor *pix_fmt_desc;
+
+    kdu_codestream *code_stream;
+    kdu_siz_params *siz_params;
+    mem_compressed_target *target;
+    kdu_stripe_compressor *encoder;
+
+    uint8_t* data;
+    uint8_t* buffer;
+    int nb_pixels;
+    int* stripe_heights;
+
+    int stop;
     int ret;
 
-    // TODO encode frame stripes
+    if ((ret = kdu_siz_params_new(&siz_params))) {
+        goto done;
+    }
+
+    pix_fmt_desc = av_pix_fmt_desc_get(avctx->pix_fmt);
+
+    // Initialize input data buffer
+    nb_pixels = frame->width * frame->height * pix_fmt_desc->nb_components;
+    data = av_malloc(nb_pixels);
+    libkdu_copy_from_packed_8(data, frame, pix_fmt_desc->nb_components);
+
+    kdu_siz_params_set_num_components(siz_params, pix_fmt_desc->nb_components);
+    kdu_siz_params_set_precision(siz_params, 0, av_get_bits_per_pixel(pix_fmt_desc));
+    kdu_siz_params_set_size(siz_params, 0, avctx->height, avctx->width);
+    kdu_siz_params_set_signed(siz_params, 0, 0);
+
+    // Allocate output buffer and code stream
+    if((ret = kdu_compressed_target_mem_new(&target))) {
+        goto done;
+    }
+
+    if ((ret = kdu_codestream_create_from_target(target, siz_params, &code_stream))) {
+        goto done;
+    };
+
+    if((ret = kdu_codestream_parse_params(code_stream, "Cmodes=HT"))) {
+        goto done;
+    }
+
+    // Create encoder
+    if ((ret = kdu_stripe_compressor_new(&encoder))) {
+        goto done;
+    }
+
+    stripe_heights = av_malloc(pix_fmt_desc->nb_components);
+    for (int i = 0; i < pix_fmt_desc->nb_components; ++i) {
+        stripe_heights[i] = avctx->height;
+    }
+
+    kdu_stripe_compressor_start(encoder, code_stream);
+
+    stop = 0;
+    while (!stop) {
+        stop = kdu_stripe_compressor_push_stripe(encoder, data, stripe_heights);
+    }
+
+    if ((ret = kdu_stripe_compressor_finish(encoder))) {
+        goto done;
+    }
+
+    kdu_compressed_target_bytes(target, &buffer, &pkt->size);
+
+    av_packet_from_data(pkt, buffer, pkt->size);
+    *got_packet = 1;
 
 done:
-    // TODO clean
+    av_free(data);
+    kdu_stripe_compressor_delete(encoder);
+    kdu_codestream_delete(code_stream);
+    kdu_compressed_target_mem_delete(target);
+    kdu_siz_params_delete(siz_params);
     return ret;
 }
 
@@ -58,7 +146,7 @@ const FFCodec ff_libkdu_encoder = {
     .encode2        = libkdu_encode_frame,
     .p.capabilities = AV_CODEC_CAP_FRAME_THREADS,
     .p.pix_fmts     = (const enum AVPixelFormat[]) {
-        AV_PIX_FMT_RGB24,
+        AV_PIX_FMT_RGB24, AV_PIX_FMT_NONE
     },
     .p.priv_class   = &kakadu_encoder_class,
     .p.wrapper_name = "libkdu",
