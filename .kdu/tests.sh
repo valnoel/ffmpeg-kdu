038008668232a84663776858ad81755635d78f57
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-05-05 09:39:00 -0700

    Improve tests

--- .kdu/tests.sh
+++ .kdu/tests.sh
@@ -3,14 +3,14 @@
 KDU_TEST_DIR=$1
 BUILD_DIR=$2/test-output
 
-FFMPEG_CMD="./ffmpeg -hide_banner -loglevel error"
+FFMPEG_CMD="./ffmpeg -y -hide_banner -loglevel error"
 
 mkdir -p ${BUILD_DIR}
 
-outfile=${BUILD_DIR}/000001_kdu_usage_examples_kdu_compress_a.j2c
-${FFMPEG_CMD} -i ${KDU_TEST_DIR}/source/meridian_216p.tif -c:v libkdu -kdu_params "Qfactor=85" ${outfile}
-cmp ${KDU_TEST_DIR}/compressed/000001_kdu_usage_examples_kdu_compress_a.j2c ${outfile}
+out_name=000001_kdu_usage_examples_kdu_compress_a.j2c
+${FFMPEG_CMD} -i ${KDU_TEST_DIR}/source/meridian_216p.tif -c:v libkdu -rate 1 ${BUILD_DIR}/${out_name}
+cmp ${KDU_TEST_DIR}/compressed/${out_name} ${BUILD_DIR}/${out_name}
 
-outfile=${BUILD_DIR}/000001_kdu_usage_examples_kdu_compress_a.tif
-${FFMPEG_CMD} -c:v libkdu -i ${KDU_TEST_DIR}/compressed/000001_kdu_usage_examples_kdu_compress_a.j2c ${outfile}
-tiffcmp -t ${KDU_TEST_DIR}/decompressed/000001_kdu_usage_examples_kdu_compress_a.tif ${outfile}
+out_name=000001_kdu_usage_examples_kdu_compress_a.tif
+${FFMPEG_CMD} -c:v libkdu -i ${KDU_TEST_DIR}/compressed/000001_kdu_usage_examples_kdu_compress_a.j2c ${BUILD_DIR}/${out_name}
+tiffcmp -t ${KDU_TEST_DIR}/decompressed/${out_name} ${BUILD_DIR}/${out_name}
