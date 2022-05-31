cb0fcd614921012d011910d6d6f79e2b6cdf15c7
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-05-31 12:51:28 -0700

    Clean-up documentation

--- libavcodec/libkduenc.c
+++ libavcodec/libkduenc.c
@@ -1,6 +1,29 @@
+/*
+ * Copyright (C) 2022 Sandflow Consulting LLC
+ *
+ * This file is part of FFmpeg-kdu.
+ *
+ * FFmpeg-kdu is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg-kdu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 /**
+ * JPEG 2000 encoder using Kakadu SDK
+ * 
  * @file
- * JPEG 2000 encoder using Kakadu
+ * @author Valentin Noel
+ * @ingroup libkdu
  */
 
 #include "libavutil/common.h"
